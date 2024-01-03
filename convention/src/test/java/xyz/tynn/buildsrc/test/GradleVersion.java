42ae73d6d4212650acb7f54900bcf3b6e8a8b100
Christian Schmitz <christian@tynn.xyz>
2024-01-03 11:13:54 +0100

    feat(convention): configure s01 nexus host as a default

--- convention/src/test/java/xyz/tynn/buildsrc/test/GradleVersion.java
+++ convention/src/test/java/xyz/tynn/buildsrc/test/GradleVersion.java
@@ -7,10 +7,14 @@ import static org.gradle.util.internal.DefaultGradleVersion.current;
 
 @SuppressWarnings("unused")
 public enum GradleVersion {
-    GRADLE_7_6("7.6.1"),
+    GRADLE_7_6("7.6.3"),
     GRADLE_8_0("8.0.2"),
-    GRADLE_8_1("8.1"),
-    LATEST("8.2-RC") {
+    GRADLE_8_1("8.1.1"),
+    GRADLE_8_2("8.2.1"),
+    GRADLE_8_3("8.3"),
+    GRADLE_8_4("8.4"),
+    GRADLE_8_5("8.5"),
+    LATEST("8.6-RC") {
         @Override
         public String toString() {
             return current().getVersion();
